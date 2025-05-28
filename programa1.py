b89510af9e1ed14ef9b156225128abbd23f40ad1
sagonzalezuach <sagonzalez@uach.mx>
2025-05-28 17:19:13 -0600

    Update programa1.py

--- programa1.py
+++ programa1.py
@@ -20,13 +20,5 @@ if st.checkbox("¿Te gusta Streamlit?"):
     st.balloons()
 
 # Imagen de prueba (requiere que subas una o pongas una URL)
-st.image("https://streamlit.io/images/brand/streamlit-logo-primary-colormark-darktext.png", width=300)
+st.image("https://www.bing.com/images/search?q=log%20uach%20png&view=detailv2&FORM=IQFRBA&id=6EDE5AD2B1A0FF3ECD7808E3E22E264E6E89CD58&selectedindex=0&&expw=900&exph=900&ccid=o8RPFiIT&ck=98668311C94862F3E212D3D4CD6B0A7E&simid=608045775415755198&thid=OIP.o8RPFiITz0fEQ5swYisj1gHaHa&idpp=serp&idpbck=1", width=300)
 
-# Gráfico simple con matplotlib
-import matplotlib.pyplot as plt
-import numpy as np
-x = np.linspace(0, 10, 100)
-y = np.sin(x)
-fig, ax = plt.subplots()
-ax.plot(x, y)
-st.pyplot(fig)
